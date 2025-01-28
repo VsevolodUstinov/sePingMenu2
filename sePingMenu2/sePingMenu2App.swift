81036a7c57a8bdeb25f0d7dcfa42eb8de4210439
VsevolodUstinov <sevaustinovpersonal@gmail.com>
2025-01-27 21:24:58 -0800

    Initial Commit

---
+++ sePingMenu2/sePingMenu2App.swift
@@ -0,0 +1,17 @@
+//
+//  sePingMenu2App.swift
+//  sePingMenu2
+//
+//  Created by Seva Ustinov on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct sePingMenu2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
